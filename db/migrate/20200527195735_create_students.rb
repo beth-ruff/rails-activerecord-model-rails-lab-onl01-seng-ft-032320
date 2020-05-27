086176e12c0c4fccc15eb649c4fa9121e4e7fc39
Beth Ruff <beth.ruff2@gmail.com>
2020-05-27 19:58:12 +0000

    Automatically backed up by Learn

--- db/migrate/20200527195735_create_students.rb
+++ db/migrate/20200527195735_create_students.rb
@@ -3,7 +3,6 @@ class CreateStudents < ActiveRecord::Migration[5.0]
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-      t.string :string
 
       t.timestamps
     end
