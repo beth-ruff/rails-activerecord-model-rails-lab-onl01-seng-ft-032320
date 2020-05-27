ed2d63d760cf27a4aa63b7408d8aa09df2b9d2ed
Beth Ruff <beth.ruff2@gmail.com>
2020-05-27 20:04:31 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,8 @@
 class Student < ApplicationRecord
   
   def to_s
-    @student = Student.create
+    @student = Student.create(first_name: Daenerys, last_name: Targaryen)
+    @student = Student.first_name.to_s
   end 
   
 end
